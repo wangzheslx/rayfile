1d170dc3a1156077a71d836986baf2f9b3695700
wangzheslx <wangzheslx@gmail.com>
2024-03-06 16:45:05 +0800

    客户端封装代码
    ResHandle

---
+++ client/reshandler.cpp
@@ -0,0 +1,6 @@
+#include "reshandler.h"
+
+ResHandler::ResHandler()
+{
+
+}
---
+++ client/reshandler.h
@@ -0,0 +1,11 @@
+#ifndef RESHANDLER_H
+#define RESHANDLER_H
+
+
+class ResHandler
+{
+public:
+    ResHandler();
+};
+
+#endif // RESHANDLER_H
